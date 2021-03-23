99dde3edb5e2a94beec1e3b6c118b14555c748b0
CryoRenegade <jledbetter460@gmail.com>
2021-03-23 17:58:46 -0600

    commit on master on 3/23/2021 5:58

--- lab8/cesaro.py
+++ lab8/cesaro.py
@@ -1,27 +1,27 @@
 import turtle
 
-def draw_cesaro_line(turtle, level, length):
+def line(turtle, level, length):
     if level == 0:
         turtle.forward(length)
     if level != 0:
-        draw_cesaro_line(turtle, level-1, length/4)
+        line(turtle, level-1, length/4)
         turtle.right(85)
-        draw_cesaro_line(turtle, level-1, length/4)
+        line(turtle, level-1, length/4)
         turtle.left(170)
-        draw_cesaro_line(turtle, level-1, length/4)
+        line(turtle, level-1, length/4)
         turtle.right(85)
-        draw_cesaro_line(turtle, level-1, length/4)
-def draw_cesaro_fractal(turtle, level, length):
-    for i in range(50):
-        draw_cesaro_line(turtle, level, length)
+        line(turtle, level-1, length/4)
+def fractal(turtle, deg, length):
+    for i in range(4):
+        line(turtle, deg, length)
         turtle.right(90)
 
 def main():
     window = turtle.Screen()
     t1 = turtle.Turtle()
     t1.speed('fastest')
-    t1.begin_fill()
-    draw_cesaro_fractal(t1, 3, 2000)
+    fractal(t1, int(input("What degree do you want the turtle to draw? ")), int(input("And how far? ")))
     t1.end_fill()
+    window.exitonclick()
 if __name__ == "__main__":
     main()
